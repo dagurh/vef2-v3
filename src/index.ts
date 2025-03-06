53218f0bda3369907163af0405fd22d0f4868290
Dagur Hardarson <201810188@post.au.dk>
2025-03-06 23:46:34 +0100

    cors added

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { createCategory, deleteCategory, getCategories, getCategory, updateCategory, validateCategory } from './categories.db.js'
+import { cors } from 'hono/cors';
 
 const app = new Hono()
+app.use('*', cors());
 
 app.get('/', (c) => {
   const services = {
